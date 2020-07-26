3c7ff5817edcfc52c4edae7a3cee103dec575024
ArtiFisher <artemis.fisher@gmail.com>
2020-07-26 11:03:19 +0300

    Add task1.1

---
+++ Task1/1.1/index.js
@@ -0,0 +1,19 @@
+const { Transform } = require('stream');
+
+const reverser = new Transform ({
+  transform: (data, encoding, callback) => {
+    callback(
+      null,
+      data.
+      toString('utf8')
+      .replace(/\n/, "") // excludes input \n from reverse process
+      .split("")
+      .reverse()
+      .join("")
+      .concat('\n\n')) // adds a few new lines to separate inputs
+  }
+})
+
+process.stdin
+  .pipe(reverser)
+  .pipe(process.stdout);
