95a7ddd9f708791d27e1f1cc1c533f0e733916e0
ArtiFisher <artemis.fisher@gmail.com>
2020-07-29 12:34:22 +0300

    Remove fs reading

--- Task1/1.2/RAMfile.js
+++ Task1/1.2/RAMfile.js
@@ -5,13 +5,11 @@ const filename = 'Task1/1.2/csv/Example'
 const input = `${filename}.csv`
 const output = `${filename}.txt`
 
-fs.readFile(input, function (err, data) {
-    if (err) throw err;
-    csv()
-    .fromFile(input)
-    .then(data => {
-        const writer = fs.createWriteStream(output)
-        writer.on('error', console.error)
-        data.forEach(line => writer.write(JSON.stringify(line) + '\n'))
-    })
-});
+csv()
+.on('error', console.error)
+.fromFile(input)
+.then(data => {
+    const writer = fs.createWriteStream(output)
+    writer.on('error', console.error)
+    data.forEach(line => writer.write(JSON.stringify(line) + '\n'))
+})
