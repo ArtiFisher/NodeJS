2808fc83a513e6e90722653220a6e438b2240ccd
ArtiFisher <artemis.fisher@gmail.com>
2020-08-04 19:00:35 +0300

    Use format transforming stream

--- Task1/1.2/RAMfile.js
+++ Task1/1.2/RAMfile.js
@@ -1,12 +1,22 @@
 import csv from 'csvtojson';
 import fs from 'fs';
 import { input, output } from './config.js';
+import { PassThrough } from 'stream';
+import formatter from './formatter.js';
+
+const dataFeed = new PassThrough()
+const writeStream = fs.createWriteStream(output)
 
 csv()
 .on('error', console.error)
 .fromFile(input)
 .then(data => {
-    const writer = fs.createWriteStream(output)
-    writer.on('error', console.error)
-    data.forEach(line => writer.write(JSON.stringify(line) + '\n'))
+    data.forEach(item => dataFeed.push(JSON.stringify(item))) // feed each chunk of data to pipe
+
+    dataFeed
+        .on('error', console.log)
+        .pipe(formatter)
+        .on('error', console.log)
+        .pipe(writeStream)
+        .on('error', console.log);
 })
--- Task1/1.2/pipeline.js
+++ Task1/1.2/pipeline.js
@@ -1,13 +1,16 @@
 import csv from 'csvtojson';
 import fs from 'fs';
 import { input, output } from './config.js';
+import formatter from './formatter.js';
 
 const readStream = fs.createReadStream(input);
 const writeStream = fs.createWriteStream(output);
 
 readStream
-.on('error', console.log)
-.pipe(csv())
-.on('error', console.log)
-.pipe(writeStream)
-.on('error', console.log);
+    .on('error', console.log)
+    .pipe(csv())
+    .on('error', console.log)
+    .pipe(formatter)
+    .on('error', console.log)
+    .pipe(writeStream)
+    .on('error', console.log);
