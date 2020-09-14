8170bcc946b86025517aa32685603c278ff4633c
ArtiFisher <artemis.fisher@gmail.com>
2020-09-14 17:04:18 +0300

    Refactored group router

--- src/api/routes/group.ts
+++ src/api/routes/group.ts
@@ -6,33 +6,38 @@ import group from '../../models/group';
 
 const validator = createValidator();
 
-const route = Router();
+const router = Router();
 
-export default (app: Router) => {
-    app.use('/group', route);
-
-    route.post('/',
-        validator.body(group.schema),
+router.route('/group')
+    .post(validator.body(group.schema),
         async (req, res) => {
             await group.data.add(req.body);
             res.sendStatus(HttpStatus.CREATED);
         })
-        .get('/', async (req: Request, res: Response) => {
-            const result = await group.data.getAll();
-            res.json(result);
-        })
-        .get('/:uid', async (req: Request, res: Response) => {
-            const result = await group.data.getById(req.params.uid);
-            res.json(result);
-        })
-        .patch('/:uid',
-            validator.body(group.schema),
-            async (req: Request, res: Response) => {
-                await group.data.change(req.params.uid, req.body);
-                res.sendStatus(HttpStatus.ACCEPTED);
-            })
-        .delete('/:uid', async (req: Request, res: Response) => {
-            await group.data.delete(req.params.uid);
+    .get(async (req: Request, res: Response) => {
+        const result = await group.data.getAll();
+        res.json(result);
+    });
+
+router.route('/:uid')
+    .get(async (req: Request, res: Response) => {
+        const result = await group.data.getById(req.params.uid);
+        res.json(result);
+    })
+    .patch(validator.body(group.schema),
+        async (req: Request, res: Response) => {
+            await group.data.change(req.params.uid, req.body);
             res.sendStatus(HttpStatus.ACCEPTED);
-        });
-};
+        })
+    .delete(async (req: Request, res: Response) => {
+        await group.data.delete(req.params.uid);
+        res.sendStatus(HttpStatus.ACCEPTED);
+    });
+
+router.route('/:uid/addUsers')
+    .post(async (req: Request, res: Response) => {
+        await group.data.addUsers(parseInt(req.params.uid, 10), req.body.users);
+        res.sendStatus(HttpStatus.ACCEPTED);
+    });
+
+export default router;
