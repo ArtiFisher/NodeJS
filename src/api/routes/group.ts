23fb8d61219b93830b3ba37ec84ea74a10673765
ArtiFisher <artemis.fisher@gmail.com>
2020-09-14 17:57:02 +0300

    Add error handling

--- src/api/routes/group.ts
+++ src/api/routes/group.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import { Router, Request, Response } from 'express';
 import * as HttpStatus from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
 import { createValidator } from 'express-joi-validation';
 import group from '../../models/group';
 
@@ -10,34 +11,34 @@ const router = Router();
 
 router.route('/')
     .post(validator.body(group.schema),
-        async (req, res) => {
+        asyncHandler(async (req, res) => {
             await group.data.add(req.body);
             res.sendStatus(HttpStatus.CREATED);
-        })
-    .get(async (req: Request, res: Response) => {
+        }))
+    .get(asyncHandler(async (req: Request, res: Response) => {
         const result = await group.data.getAll();
         res.json(result);
-    });
+    }));
 
 router.route('/:uid')
-    .get(async (req: Request, res: Response) => {
+    .get(asyncHandler(async (req: Request, res: Response) => {
         const result = await group.data.getById(req.params.uid);
         res.json(result);
-    })
+    }))
     .patch(validator.body(group.schema),
-        async (req: Request, res: Response) => {
+        asyncHandler(async (req: Request, res: Response) => {
             await group.data.change(req.params.uid, req.body);
             res.sendStatus(HttpStatus.ACCEPTED);
-        })
-    .delete(async (req: Request, res: Response) => {
+        }))
+    .delete(asyncHandler(async (req: Request, res: Response) => {
         await group.data.delete(req.params.uid);
         res.sendStatus(HttpStatus.ACCEPTED);
-    });
+    }));
 
 router.route('/:uid/addUsers')
-    .post(async (req: Request, res: Response) => {
+    .post(asyncHandler(async (req: Request, res: Response) => {
         await group.data.addUsers(parseInt(req.params.uid, 10), req.body.users);
         res.sendStatus(HttpStatus.ACCEPTED);
-    });
+    }));
 
 export default router;
