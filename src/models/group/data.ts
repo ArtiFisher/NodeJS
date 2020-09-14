07171bf032a8197ae0eec208aae5275846314eda
ArtiFisher <artemis.fisher@gmail.com>
2020-09-14 17:04:44 +0300

    Add users adding method

--- src/models/group/data.ts
+++ src/models/group/data.ts
@@ -9,6 +9,13 @@ class Groups {
             .insert(body);
     }
 
+    addUsers(group_id: number, userIds: Array<number>) {
+        return database.transaction(async trx => {
+            const inserts = await trx('usergroup').insert(userIds.map(user_id => ({ group_id, user_id })));
+            console.log(`${inserts.length} users added.`);
+        });
+    }
+
     getAll() {
         return database('groups');
     }
